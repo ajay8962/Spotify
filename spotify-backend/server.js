fc2fd84877784ff4a9ad9f65294a351f9f3fa300
Ajay Dangi <ajaydangi233@gmail.com>
2024-07-15 16:11:03 +0530

    commit

---
+++ spotify-backend/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import songRouter from './src/routes/songRoute.js';
+import connectDB from './src/config/mongodb.js';
+import connectCloudinary from './src/config/cloudinary.js';
+import albumRouter from './src/routes/albumRoute.js';
+
+
+// app config
+const app = express();
+const port = process.env.PORT ;
+connectDB();
+connectCloudinary();
+
+// middleware
+app.use(express.json());
+app.use(cors({
+    origin: [""],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
+
+
+
+//initialize the app
+app.use('/api/song', songRouter);
+app.use('/api/album', albumRouter);
+
+app.get('/', (req, res) => res.send("API is running"));
+
+
+// listen
+app.listen(port, () => console.log(`Listening on localhost:${port}`));
