88e8e53e1dadc9528c15917644c205f3936ad6a9
Ajay Dangi <ajaydangi233@gmail.com>
2024-07-15 16:32:41 +0530

    commit

--- spotify-backend/server.js
+++ spotify-backend/server.js
@@ -14,9 +14,12 @@ connectDB();
 connectCloudinary();
 
 // middleware
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
-
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}))
 
 
 
